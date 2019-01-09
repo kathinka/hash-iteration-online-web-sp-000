d45cec805b8d20eedeb70b180c51c7e7a2bd3a63
kathinka martinsen <kathinkamartinsen@gmail.com>
2019-01-09 15:41:37 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -16,7 +16,7 @@ end
 winner
 end
 
-def happt_birthday(birthday_kids)
+def happy_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
 end
